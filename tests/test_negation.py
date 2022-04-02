e8ca4db1ee4d4059891f5157f14a5eb2cbffe342
Paul Bricman <paulbricman@protonmail.com>
2022-04-02 08:56:02 +0200

    fix: negation test

--- tests/test_negation.py
+++ tests/test_negation.py
@@ -2,5 +2,5 @@ from src.negation import negate
 
 
 def test_easy():
-    assert 'This is not how I want news to look like.' in negate(
-        'This is how I want news to look like.')
+    assert 'Cooking is easy.' in negate(
+        'Cooking is hard.')
