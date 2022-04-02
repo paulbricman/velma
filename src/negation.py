fb88a270831cf7488e5150f72f70c74dd4c6e556
Paul Bricman <paulbricman@protonmail.com>
2022-04-02 14:10:41 +0200

    feat: implement filter_negations

--- src/negation.py
+++ src/negation.py
@@ -1,9 +1,22 @@
-from transformers import pipeline
 from transformers import AutoTokenizer, AutoModelForCausalLM
+from sentence_transformers import CrossEncoder
 from pathlib import Path
+from textdistance import levenshtein
 
 
-def negate(statement, candidates=2, contradiction_threshold=0.9, edit_threshold=3, lm=None, lm_tok=None, nli=None):
+def gen_negations(statement, candidates=2, lm=None, lm_tok=None):
+    '''
+    Produce a list of candidates negations of a given statement.
+
+    Args:
+        statement (str): The statement to negate.
+        candidates (int): The number of candidates to return.
+        lm (str): The language model name.
+        lm_tok (str): The language model tokenizer name.
+
+    Returns:
+        list: A list of candidates negations of the given statement.
+    '''
     assert statement and statement != '', 'Statement is empty.'
 
     if not lm:
@@ -11,10 +24,6 @@ def negate(statement, candidates=2, contradiction_threshold=0.9, edit_threshold=
 
     if not lm_tok:
         lm_tok = AutoTokenizer.from_pretrained('EleutherAI/gpt-neo-1.3B')
-
-    # if not nli:
-    #     nli = pipeline('zero-shot-classification',
-    #                    model='cross-encoder/nli-distilroberta-base')
 
     prompt_path = Path('data') / 'negation_prompt.txt'
     if not prompt_path.exists():
@@ -43,6 +52,53 @@ def negate(statement, candidates=2, contradiction_threshold=0.9, edit_threshold=
 
 
 def force_eos(completion_ids):
+    '''
+    Force text generation to stop after newline.
+
+    Args:
+        completion_ids (list): The list of previous token ids.
+
+    Returns:
+        list: The list of legal token ids.
+    '''
     if completion_ids[-1] in [628, 198]:
         return [50256]
     return range(50256)
+
+
+def filter_negations(statement, candidates, nli=None, lm_tok=None, nli_threshold=0.9, edit_threshold=3):
+    '''
+    Filter candidates to remove those that are not negations of the given statement. Also, encourage minimal edits.
+
+    Args:
+        candidates (list): A list of candidates to filter.
+        statement (str): The statement to negate.
+        nli (str): The NLI model name.
+
+    Returns:
+        list: A list of filtered candidates.
+    '''
+    assert statement and statement != '', 'Statement is empty.'
+
+    if not nli:
+        nli = CrossEncoder('cross-encoder/nli-deberta-v3-base')
+
+    if not lm_tok:
+        lm_tok = AutoTokenizer.from_pretrained('EleutherAI/gpt-neo-1.3B')
+
+    pairs = [(statement, e) for e in candidates]
+    scores = nli.predict(pairs, apply_softmax=True)
+    nli_filtered = [e for e_id, e in enumerate(
+        candidates) if scores[e_id][0] > nli_threshold]
+
+    # Batching would involve handling padding tokens properly...
+    candidate_ids = [lm_tok(e, return_tensors='pt')['input_ids'][0].tolist()
+                     for e in nli_filtered]
+    statement_ids = lm_tok(statement, return_tensors='pt')[
+        'input_ids'][0].tolist()
+    edit_dists = [levenshtein.distance(statement_ids, e)
+                  for e in candidate_ids]
+    edit_filtered = [e for e_id, e in enumerate(
+        nli_filtered) if edit_dists[e_id] <= edit_threshold]
+
+    return edit_filtered
--- tests/test_negation.py
+++ tests/test_negation.py
@@ -1,6 +1,17 @@
-from src.negation import negate
+from src.negation import gen_negations, filter_negations
 
 
-def test_easy():
-    assert 'Cooking is easy.' in negate(
+def test_gen_negations():
+    assert 'Cooking is easy.' in gen_negations(
         'Cooking is hard.')
+
+
+def test_filter_negations():
+    statement = 'Cooking is hard.'
+    candidates = ['Cooking is easy.', 'Cooking is difficult.',
+                  'Given this, the practice of cooking is absolutely trivial.']
+    filtered = filter_negations(statement, candidates)
+
+    assert 'Cooking is easy.' in filtered
+    assert 'Cooking is difficult.' not in filtered
+    assert 'Given this, the practice of cooking is absolutely trivial.' not in filtered
